a8fb64abcb4e53821a1b5be58952ad6bfc0c5f6b
Danil Tolstov <dfrontman@gmail.com>
2022-05-21 15:27:59 +0500

    add base routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './scss/app.scss';
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
+import {Route, Routes} from "react-router-dom";
+import NotFound from "./pages/NotFound";
+
 function App() {
 
   return (
@@ -11,6 +14,10 @@ function App() {
         <Header />
         <div className="content">
           <div className="container">
+              <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+              </Routes>
             <Home />
           </div>
         </div>
